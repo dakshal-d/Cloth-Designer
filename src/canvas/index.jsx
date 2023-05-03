706b23573e5312c83e80c7e92830e0b23e8fd7d6
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-05-03 23:34:30 +0530

    init

---
+++ src/canvas/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+import Shirt from './Shirt'
+import CamRig from './CamRig'
+import Backdrop from './Backdrop'
+const CanvasModel1 = () => {
+  return (
+    <Canvas
+    shadows
+    camera={{position:[0,0,0], fov:25}}
+    gl={{preserveDrawingBuffer: true}}
+    className='w-full max-w-full h-full transition-all ease-in'
+    >
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/> 
+
+      <CamRig>
+        <Backdrop/>
+        <Center>
+          <Shirt />
+        </Center>
+      </CamRig>   
+      </Canvas>
+  )
+}
+
+export default CanvasModel1
